82ad81e1abcd4bd6044ca14d3930d26bdc3eb009
joackson <joackson@gmail.com>
2019-01-20 20:31:49 -0200

    Chamada da api alpha vantage

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,9 @@
 'use strict';
+
+const https = require('https');
+
+const baseURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&interval=15min&outputsize=full&apikey=TJAVUEK5TU2C253U&dataType=json&outputsize=compact';
+
 module.exports = function(app) {
 
   app.route('/api/carteira').get((req, res) => {
@@ -20,5 +25,29 @@ module.exports = function(app) {
                 quantidade: 100
             }
         ]);
-    })
+    });
+
+    app.route('/api/consultar/:papel').get((req, res) => {
+        let url = baseURL + '&symbol=' + req.params.papel
+        console.log('URL ' + url)
+        https.get(url, (resp) => {
+            let data = '';
+          
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                console.log(chunk)
+              data += chunk;
+            });
+          
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log('Data -> ' + data)
+                res.send(data)
+ //             console.log(JSON.parse(data).explanation);
+            });
+          
+          }).on("error", (err) => {
+            console.log("Error: " + err.message);
+          });
+    })   
 };
